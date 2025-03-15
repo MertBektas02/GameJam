0d254f143c07494ff327aef5ea721e0ab6e5b9fa
Çağatay Bayram GÖNEN <cagataybayramgonen@gmail.com>
2025-03-15 15:04:27 +0300

    FIX: merge conflict between main and cagatay_dev

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -2,28 +2,26 @@ using UnityEngine;
 
 public class PlanetGenerator : MonoBehaviour
 {
-    public Material planetMaterial; // material that will be assigned to the planet.
+    public Material baseMaterial;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void GeneratePlanet()
     {
-        GeneratePlanet();
-    }
-
-    void GeneratePlanet()
-    {
-        float randomSize = Random.Range(0.5f, 2f); // Random size
-        transform.localScale = Vector3.one * randomSize; // changing the size of planet according to randomSize.
-
-        if(planetMaterial != null)
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        if (renderer == null)
         {
-            Color randomColor = new Color(Random.value, Random.value, Random.value); // random color
-            planetMaterial.color = randomColor; // signing the random color to the planetMaterial.
-            GetComponent<MeshRenderer>().material = planetMaterial; // signing planetMaterial to the obj meshrenderer.
+            Debug.LogWarning("MeshRenderer bulunamadý!");
+            return;
         }
 
-        transform.rotation = Random.rotation; // randomizing the rotation of the obj.
-    }
+        Material newMaterial = new Material(baseMaterial); // Materyali kopyala, böylece deðiþiklik tüm gezegenlere yayýlmaz
+        newMaterial.SetColor("_BaseColor", new Color(Random.value, Random.value, Random.value));
+        newMaterial.SetFloat("_NoiseScale", Random.Range(10f, 100f));
+        newMaterial.SetFloat("_NoiseStrength", Random.Range(0.1f, 1f));
+        newMaterial.SetFloat("_FresnelIntensity", Random.Range(0.5f, 3f));
 
-    
+        renderer.material = newMaterial;
+
+        float randomSize = Random.Range(0.5f, 2f);
+        transform.localScale = Vector3.one * randomSize;
+    }
 }
